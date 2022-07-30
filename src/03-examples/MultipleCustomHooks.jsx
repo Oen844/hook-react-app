6fcdf476f64aaae7da348a187f73b20887e74e02
Pau Egea <papau84@gmail.com>
2022-07-30 19:25:53 +0200

    useFetch + useCounter

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -4,14 +4,36 @@ import { useFetch } from '../hooks/useFetch'
 
 export const MultipleCustomHooks = () => {
 
-    const { data, isloading, hasError } = useFetch('https://www.breakingbadapi.com/api/quotes/1')
+  const { data, isloading, hasError } = useFetch('https://www.breakingbadapi.com/api/quotes/1')
+
+  const { author, quote } = !!data && data[0];
 
 
-    
+  if (isloading) {
+
+  }
+
   return (
-   <>
-    <h1>Breaking Bad Quotes</h1>
-    <hr/>
-   </>
+    <>
+      <h1>Breaking Bad Quotes</h1>
+      <hr />
+
+      {
+        isloading ? (
+          <div className='alert alert-info text-center'>
+            Loading...
+          </div>
+        )
+          : (
+            <blockquote className='blockquote text-end'>
+              <p className='mb-1'>{quote}</p>
+              <footer className='blockquote-footer'> {author}</footer>
+            </blockquote>
+          )
+      }
+
+
+
+    </>
   )
 }
