758a9c2efa0ad86abab8937883ebf29800434caa
Pau Egea <papau84@gmail.com>
2022-08-01 16:39:58 +0200

    memo react

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import SimpleForm from './02-useEffect/SimpleForm'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayaoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 import { HooksApp } from './HooksApp'
 
 import './index.css'
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <FormWithCustomHook/> */}
     {/* <MultipleCustomHooks/> */}
     {/* <FocusScreen/> */}
-    <Layout/>
+    {/* <Layout/> */}
+    <Memorize/>
   </React.StrictMode>
 )
